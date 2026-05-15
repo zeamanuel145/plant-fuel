913aef3c211f588c2531097a3d1b6062f4f6c4ad
zeamanuel145 <147749691+zeamanuel145@users.noreply.github.com>
2026-05-15 20:38:32 +0300

    Update home.script.js

--- js/home.script.js
+++ js/home.script.js
@@ -1,4 +1,4 @@
-// Thedhhgggghyuuuuhehehew
+// Thedhhgggghyuuuuhehehe
 functioninitTheme() {
     // Checkfor saved theme reference or use device preference
     const savedTheme = localStorage.getItem('theme');
